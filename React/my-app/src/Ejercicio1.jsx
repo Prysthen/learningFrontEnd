771f05bd296e832b3c59bd3b238b79f66770a503
Prysthen <saraqs.96@hotmail.com>
2025-03-19 14:49:43 +0100

    -Frist commit. Starting React.

---
+++ React/my-app/src/Ejercicio1.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola, Ract en visual Studio Code!</h1>
+      <p>Esto es un componenete React básico.</p>
+    </div>
+  );
+}
+export default App;
+
+/* Extensiones a instalar en Visual: Prettier Code Formater, ES7+ React, Indent Rainbow. Instalar en Navegador: React Developer Tools */
